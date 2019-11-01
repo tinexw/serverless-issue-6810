59beabe03f6d454e4c1513f6a45aa2377bda633d
Kristine Jetzke <kristine.jetzke@kreuzwerker.de>
2019-11-01 21:39:49 +0100

    Nicer output

--- script.sh
+++ script.sh
@@ -9,43 +9,61 @@ rootId=`aws cloudformation list-exports  --output text --no-paginate --query $qu
 rm -rf resources-*.yml 
 
 # Deploy /bar - 1st run
-echo "Deploying /bar - 1st run"
-aws apigateway get-resources  --rest-api-id $rootId --query=items[].[path,id]  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'  > resources-service-bar.yml 
+echo "==== Deploying bar - 1st run ===="
+aws apigateway get-resources  --rest-api-id $rootId --query=items[].[path,id]  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'   > resources-service-bar.yml 
+echo "-- apiGateway.restApiResources --"
 cat resources-service-bar.yml 
+echo "---------------------------------"
 sls deploy  --config service-bar.yaml
-echo "Resources after deploying /bar - 1st run:"
+echo "------ Available resources ------"
 aws apigateway get-resources --rest-api-id $rootId --query=items[].[path,id] --output text
+echo "---------------------------------"
+echo "================================="
+
 
 rm -rf resources-*.yml 
 
 # Deploy /foo
-echo "Deploying /foo"
-aws apigateway get-resources  --rest-api-id $rootId --query=items[].[path,id]  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'  > resources-service-foo.yml 
+echo "========= Deploying foo ========="
+aws apigateway get-resources  --rest-api-id $rootId --query=items[].[path,id]  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'   > resources-service-foo.yml 
+echo "-- apiGateway.restApiResources --"
 cat resources-service-foo.yml 
+echo "---------------------------------"
 sls deploy  --config service-foo.yaml
-echo "Resources after deploying /foo:"
+echo "------ Available resources ------"
 aws apigateway get-resources --rest-api-id $rootId --query=items[].[path,id] --output text
+echo "---------------------------------"
+echo "================================="
+
 
 rm -rf resources-*.yml 
 
 # Deploy /bar - 2nd  run
-echo "Deploying /bar - 2nd run"
-aws apigateway get-resources  --rest-api-id $rootId --query=items[].[path,id]  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'  > resources-service-bar.yml 
+echo "==== Deploying bar - 2nd run ===="
+aws apigateway get-resources  --rest-api-id $rootId --query=items[].[path,id]  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'     > resources-service-bar.yml 
+echo "-- apiGateway.restApiResources --"
 cat resources-service-bar.yml 
+echo "---------------------------------"
 sls deploy  --config service-bar.yaml
-echo "Resources after deploying /bar - 2nd run:"
+echo "------ Available resources ------"
 aws apigateway get-resources --rest-api-id $rootId --query=items[].[path,id] --output text
+echo "---------------------------------"
+echo "================================="
+
 
 rm -rf resources-*.yml 
 
 # Deploy /bar - 3rd run
-echo "/Deploying bar - 3rd run"
+echo "==== Deploying bar - 3rd run ===="
 aws apigateway get-resources  --rest-api-id $rootId --query=items[].[path,id]  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'  > resources-service-bar.yml 
+echo "-- apiGateway.restApiResources --"
 cat resources-service-bar.yml 
+echo "---------------------------------"
 sls deploy  --config service-bar.yaml
-echo "Resources after deploying /bar - 3rd run:"
+echo "------ Available resources ------"
 aws apigateway get-resources --rest-api-id $rootId --query=items[].[path,id] --output text
-
+echo "---------------------------------"
+echo "================================="
 
 serverless remove --config service-foo.yaml
 serverless remove --config service-bar.yaml
