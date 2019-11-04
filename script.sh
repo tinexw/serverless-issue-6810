4fd3ed920106fa26e9ac31ddfa62716d9e0a2be2
Kristine Jetzke <kristine.jetzke@kreuzwerker.de>
2019-11-04 11:55:35 +0100

    Add example for bug that unrelated resource gets deleted

--- script.sh
+++ script.sh
@@ -8,13 +8,26 @@ rootId=`aws cloudformation list-exports  --output text --no-paginate --query $qu
 
 rm -rf resources-*.yml 
 
-# Deploy /bar - 1st run
-echo "==== Deploying bar - 1st run ===="
-aws apigateway get-resources  --rest-api-id $rootId --query='items[?path!=`/bar`].[path,id]'  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'   > resources-service-bar.yml 
+# Deploy /bar/baz - 1st run
+echo "== Deploying bar/baz - 1st run =="
+aws apigateway get-resources  --rest-api-id $rootId --query='items[?path!=`/bar/baz`].[path,id]'  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'   > resources-service-bar-baz.yml 
 echo "-- apiGateway.restApiResources --"
-cat resources-service-bar.yml 
+cat resources-service-bar-baz.yml 
 echo "---------------------------------"
-sls deploy  --config service-bar.yaml
+sls deploy  --config service-bar-baz.yaml
+echo "------ Available resources ------"
+aws apigateway get-resources --rest-api-id $rootId --query=items[].[path,id] --output text
+echo "---------------------------------"
+echo "================================="
+
+
+# Deploy /bar/baz2 - 1st run
+echo "== Deploying bar/baz2 - 1st run =="
+aws apigateway get-resources  --rest-api-id $rootId --query='items[?path!=`/bar/baz2`].[path,id]'  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'   > resources-service-bar-baz2.yml 
+echo "-- apiGateway.restApiResources --"
+cat resources-service-bar-baz2.yml 
+echo "---------------------------------"
+sls deploy  --config service-bar-baz2.yaml
 echo "------ Available resources ------"
 aws apigateway get-resources --rest-api-id $rootId --query=items[].[path,id] --output text
 echo "---------------------------------"
@@ -38,13 +51,13 @@ echo "================================="
 
 rm -rf resources-*.yml 
 
-# Deploy /bar - 2nd  run
-echo "==== Deploying bar - 2nd run ===="
-aws apigateway get-resources  --rest-api-id $rootId --query='items[?path!=`/bar`].[path,id]'  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'     > resources-service-bar.yml 
+# Deploy /bar/baz - 2nd  run
+echo "== Deploying bar/baz - 2nd run =="
+aws apigateway get-resources  --rest-api-id $rootId --query='items[?path!=`/bar/baz`].[path,id]'  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'     > resources-service-bar-baz.yml 
 echo "-- apiGateway.restApiResources --"
-cat resources-service-bar.yml 
+cat resources-service-bar-baz.yml 
 echo "---------------------------------"
-sls deploy  --config service-bar.yaml
+sls deploy  --config service-bar-baz.yaml
 echo "------ Available resources ------"
 aws apigateway get-resources --rest-api-id $rootId --query=items[].[path,id] --output text
 echo "---------------------------------"
@@ -53,18 +66,20 @@ echo "================================="
 
 rm -rf resources-*.yml 
 
-# Deploy /bar - 3rd run
-echo "==== Deploying bar - 3rd run ===="
-aws apigateway get-resources  --rest-api-id $rootId --query='items[?path!=`/bar`].[path,id]'  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'  > resources-service-bar.yml 
+
+# Deploy /bar/baz - 3rd  run
+echo "== Deploying bar/baz - 3rd run =="
+aws apigateway get-resources  --rest-api-id $rootId --query='items[?path!=`/bar/baz`].[path,id]'  --output text | sort |  gsed '1d' | gsed 's/\t/: /' | gsed 's:^/::'     > resources-service-bar-baz.yml 
 echo "-- apiGateway.restApiResources --"
-cat resources-service-bar.yml 
+cat resources-service-bar-baz.yml 
 echo "---------------------------------"
-sls deploy  --config service-bar.yaml
+sls deploy  --config service-bar-baz.yaml
 echo "------ Available resources ------"
 aws apigateway get-resources --rest-api-id $rootId --query=items[].[path,id] --output text
 echo "---------------------------------"
 echo "================================="
 
 serverless remove --config service-foo.yaml
-serverless remove --config service-bar.yaml
+serverless remove --config service-bar-baz.yaml
+serverless remove --config service-bar-baz2.yaml
 serverless remove --config api-gateway.yaml
